b0f89e3be62d837c4f459881a2bf6fd729f89c6d
cqxmzhc <ldzslds@gmail.com>
2017-04-21 17:58:47 +0800

    1.dircolors 2.powerline-status

--- terminalconf/custom.sh
+++ terminalconf/custom.sh
@@ -1,10 +1,23 @@
-alias ls='ls --color=auto'                                                                                                                               
+# aliases
+alias ls='ls --color=auto'                                                                                                                        
+alias vb='vim ~/.bashrc'
+alias sb='source ~/.bashrc'
+alias vc='vim ~/custom.sh'
+alias j='jobs -l'
+alias f='fg %'
+alias b='bg %'
+
+# git editor
+export GIT_EDITOR=vim
 
 # customize bash prompt
-PS1='\n\e[38;5;9m\u@\h: \e[38;5;202m\w\e[35;5;0m \e[38;5;1m$(vcprompt -f %m)\e[38;5;40m$(vcprompt -f %b) \e[38;5;243m\t\e[0m\n-> '
-export GIT_EDITOR=vim
-eval `dircolors ~/.dir_colors/dircolors`
+PS1='\n\e[38;5;226m\u@\h: \e[38;5;226m\W\e[35;5;0m \e[38;5;1m$(vcprompt -f %m)\e[38;5;40m$(vcprompt -f %b) \e[38;5;253m\t\e[0m\n\e[38;5;255m-> '
 
-# customize git prompt
-GIT_PROMPT_ONLY_IN_REPO=1
-source ~/.bash-git-prompt/gitprompt.sh
+# powerline-status
+powerline-daemon -q
+POWERLINE_BASH_CONTINUATION=1
+POWERLINE_BASH_SELECT=1
+. /usr/lib/python2.7/site-packages/powerline/bindings/bash/powerline.sh
+
+# dircolors
+eval `dircolors ~/.dir_colors/dircolors`
