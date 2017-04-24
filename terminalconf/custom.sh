e49a16c8b147ac315eb124bcf88f6c8c842e1e1f
cqxmzhc <ldzslds@gmail.com>
2017-04-24 18:06:38 +0800

    powerline configuration

--- terminalconf/custom.sh
+++ terminalconf/custom.sh
@@ -11,7 +11,12 @@ alias b='bg %'
 export GIT_EDITOR=vim
 
 # customize bash prompt
-PS1='\n\e[38;5;226m\u@\h: \e[38;5;226m\W\e[35;5;0m \e[38;5;1m$(vcprompt -f %m)\e[38;5;40m$(vcprompt -f %b) \e[38;5;253m\t\e[0m\n\e[38;5;255m-> '
+export powerline_path=$(python -c 'import pkgutil; print pkgutil.get_loader("powerline").filename' 2>/dev/null)
+  if [[ "$powerline_path" != "" ]]; then
+    source ${powerline_path}/bindings/bash/powerline.sh
+  else
+	PS1='\n\e[38;5;226m\u@\h: \e[38;5;226m\W\e[35;5;0m \e[38;5;1m$(vcprompt -f %m)\e[38;5;40m$(vcprompt -f %b) \e[38;5;253m\t\e[0m\n\e[38;5;255m-> '
+  fi
 
 # powerline-status
 powerline-daemon -q
